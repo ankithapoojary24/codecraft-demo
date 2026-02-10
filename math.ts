d60de2c235fc3315540ee013ba2c07c4eab4297c
Ankitha <ankitha.poojary@codecraft.co.in>
2026-02-10 15:45:45 +0530

    sqrt implemented

--- math.ts
+++ math.ts
@@ -1,5 +1,5 @@
 /**
- * next change
+ * master changed
  * @param a1 
  * @param number 
  * @param b1 
@@ -14,6 +14,20 @@ function add(a1:number,b1:number):number{
  * @param b1 
  * @returns 
  */
-function subtract-new(a1:number,b1:number):number{
+function subtract_new(a1:number,b1:number):number{
     return a1-b1;
+}
+/**
+ * square changed
+ * @param a1 
+ * @returns 
+ */
+function square(a1:number):number{
+    return a1*a1 ;
+}
+function cube(a:number):number{
+    return a*a*a ;
+}
+function sqroot(a:number):number{
+    return Math.sqrt(a) ;
 }
